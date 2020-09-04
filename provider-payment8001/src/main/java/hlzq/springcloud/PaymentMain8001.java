51be3aab82bb2af198472bff1207cf8716bd52d6
zdj <zdjyx126@126.com>
2020-09-04 17:47:39 +0800

    修改Spring Cloud子项目3

---
+++ provider-payment8001/src/main/java/hlzq/springcloud/PaymentMain8001.java
@@ -0,0 +1,10 @@
+package hlzq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
