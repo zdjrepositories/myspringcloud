27e278e33364fbc1ace72eca8713151b121156c3
zdj <zdjyx126@126.com>
2020-09-07 14:33:11 +0800

    修改Order子项目

--- cloud-consumer-order80/src/main/java/hlzq/springcloud/cloud/contorller/OrderContorller.java
+++ cloud-consumer-order80/src/main/java/hlzq/springcloud/cloud/contorller/OrderContorller.java
@@ -1,7 +1,14 @@
 package hlzq.springcloud.cloud.contorller;
 
+import hlzq.springcloud.cloud.entities.CommonResult;
+import hlzq.springcloud.cloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * @author 张冬军
@@ -13,4 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class OrderContorller {
 
+    public static final String PAYMENT_URL="http://127.0.0.1:8001";
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> create(Payment payment){
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class );
+    }
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> getPayment(@PathVariable("id")Long id){
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class );
+    }
 }
