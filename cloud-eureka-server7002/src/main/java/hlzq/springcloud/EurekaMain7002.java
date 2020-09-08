33ff22f8feba3e003726cbd9520087d460ffe6ac
zdj <zdjyx126@126.com>
2020-09-08 12:10:31 +0800

    创建cloud-eureka-server7002项目

---
+++ cloud-eureka-server7002/src/main/java/hlzq/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package hlzq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 张冬军
+ * @version 1.0
+ * @date 2020/9/8 10:27
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
