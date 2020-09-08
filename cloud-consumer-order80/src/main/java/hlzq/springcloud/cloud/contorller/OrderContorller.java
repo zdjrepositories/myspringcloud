e9c6d049d9b5ce2e9eba8cb476caf5fb20bb47b1
zdj <zdjyx126@126.com>
2020-09-08 08:37:13 +0800

    修改重构

--- cloud-consumer-order80/src/main/java/hlzq/springcloud/cloud/contorller/OrderContorller.java
+++ cloud-consumer-order80/src/main/java/hlzq/springcloud/cloud/contorller/OrderContorller.java
@@ -20,16 +20,19 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderContorller {
 
-    public static final String PAYMENT_URL="http://127.0.0.1:8001";
+    public static final String PAYMENT_URL="http://localhost:8001";
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> create(Payment payment){
-        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class );
-    }
+
     @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+       return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class );
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id")Long id){
+        System.out.println("张冬军张冬军张冬军张冬军"+PAYMENT_URL+"/payment/get/"+id );
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class );
     }
 }
--- provider-payment8001/src/main/java/hlzq/springcloud/contriller/PeymentControl.java
+++ provider-payment8001/src/main/java/hlzq/springcloud/contriller/PeymentControl.java
@@ -5,10 +5,7 @@ import hlzq.springcloud.entities.Payment;
 import hlzq.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -29,7 +26,7 @@ public class PeymentControl {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result=paymentService.create(payment);
         log.info("****插入结果："+result);
         if(result>0){
